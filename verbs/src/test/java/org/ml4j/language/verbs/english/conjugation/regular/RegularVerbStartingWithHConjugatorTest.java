0c549e5f94b01cd8f951e1c1c1dae75ac79aaa18
Michael Lavelle <714044+michaellavelle@users.noreply.github.com>
2023-02-01 15:16:46 +0000

    Adding regular verbs starting with "h" and corresponding test

---
+++ verbs/src/test/java/org/ml4j/language/verbs/english/conjugation/regular/RegularVerbStartingWithHConjugatorTest.java
@@ -0,0 +1,14 @@
+package org.ml4j.language.verbs.english.conjugation.regular;
+
+import org.junit.jupiter.api.Test;
+import org.ml4j.language.verbs.english.EnglishWordsEnUK;
+import org.ml4j.language.verbs.english.conjugation.regular.base.RegularVerbConjugatorTestBase;
+
+public class RegularVerbStartingWithHConjugatorTest extends RegularVerbConjugatorTestBase {
+
+    @Test
+    public void testRegularVerbConjugation() {
+        testRegularVerbConjugation(EnglishWordsEnUK.VERBS_STARTING_WITH_H, EXPECTED_CONJUGATED_VERBS_STARTING_WITH_H_FILE_PATH);
+    }
+
+}
