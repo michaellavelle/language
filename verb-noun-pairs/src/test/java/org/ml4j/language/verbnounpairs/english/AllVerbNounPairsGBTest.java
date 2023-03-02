ad4da2a85e20467db735210b1f47c7b73d6791d4
Michael Lavelle <714044+michaellavelle@users.noreply.github.com>
2023-03-02 22:12:12 +0000

    Adding further pairs for 'nt' ending nouns, and some tidy up and correction of some spelling errors

---
+++ verb-noun-pairs/src/test/java/org/ml4j/language/verbnounpairs/english/AllVerbNounPairsGBTest.java
@@ -0,0 +1,12 @@
+package org.ml4j.language.verbnounpairs.english;
+
+import org.junit.jupiter.api.Test;
+import org.ml4j.language.verbnounpairs.english.base.VerbNounPairsNounsEndingWithEnGBTestBase;
+
+public class AllVerbNounPairsGBTest extends VerbNounPairsNounsEndingWithEnGBTestBase {
+
+    @Test
+    public void testVerbNounPairsLoad() {
+        testVerbNounPairsLoad("", 863);
+    }
+}
