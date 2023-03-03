97b410b483ee0d854f73861f7be52cca851e9c69
Michael Lavelle <714044+michaellavelle@users.noreply.github.com>
2023-03-03 04:43:54 +0000

    Adding first batch of verb-noun pairs where noun ends with 'er'

--- verb-noun-pairs/src/test/java/org/ml4j/language/verbnounpairs/english/VerbNounPairsNounsEndingWithErEnGBTest.java
+++ verb-noun-pairs/src/test/java/org/ml4j/language/verbnounpairs/english/VerbNounPairsNounsEndingWithErEnGBTest.java
@@ -3,10 +3,10 @@ package org.ml4j.language.verbnounpairs.english;
 import org.junit.jupiter.api.Test;
 import org.ml4j.language.verbnounpairs.english.base.VerbNounPairsNounsEndingWithEnGBTestBase;
 
-public class AllVerbNounPairsGBTest extends VerbNounPairsNounsEndingWithEnGBTestBase {
+public class VerbNounPairsNounsEndingWithErEnGBTest extends VerbNounPairsNounsEndingWithEnGBTestBase {
 
     @Test
     public void testVerbNounPairsLoad() {
-        testVerbNounPairsLoad("", 1077);
+        testVerbNounPairsLoad("er", 89);
     }
 }
